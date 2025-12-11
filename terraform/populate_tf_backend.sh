bf3648d75f780b8e979281fb0c6a21cb9f132dc8
Donald Futers <donald.futers@gmail.com>
2025-12-11 15:44:08 +0000

    🔨 update populate_tf_backend.sh to make retrieval of .tfenv's contents and seeding of target .tf.templates fully programmatic

--- terraform/populate_tf_backend.sh
+++ terraform/populate_tf_backend.sh
@@ -8,25 +8,30 @@ safe_escape () {
     printf '%s\n' "$1" | sed 's/[&/\]/\\&/g'
 }
 
-AWS_REGION_ESC=$(safe_escape "$AWS_REGION")
-STATE_BUCKET_ESC=$(safe_escape "$STATE_BUCKET")
-STATE_KEY_ESC=$(safe_escape "$STATE_KEY")
-PREFIX_ESC=$(safe_escape "$PREFIX")
-TAGS_CLIENT_ESC=$(safe_escape "$TAGS_CLIENT")
-TAGS_PROJECT_ESC=$(safe_escape "$TAGS_PROJECT")
+VARS=( $(cat "${SCRIPT_DIR}/.tfenv" | grep -Po "\b(.+)(?==)") )
+
+declare -A VALS
+for VAR in "${VARS[@]}"
+do
+    VALS+=( ["$VAR"]="$( eval "safe_escape \"\$${VAR}\"" )" )
+done
 
 generate () {
-    sed -e "s/\${tfenv.AWS_REGION}/${AWS_REGION_ESC}/g" \
-        -e "s/\${tfenv.STATE_BUCKET}/${STATE_BUCKET_ESC}/g" \
-        -e "s/\${tfenv.STATE_KEY}/${STATE_KEY_ESC}/g" \
-        -e "s/\${tfenv.PREFIX}/${PREFIX_ESC}/g" \
-        -e "s/\${tfenv.TAGS_CLIENT}/${TAGS_CLIENT_ESC}/g" \
-        -e "s/\${tfenv.TAGS_PROJECT}/${TAGS_PROJECT_ESC}/g" \
-        "${SCRIPT_DIR}/$1.tf.template" \
-        > "${SCRIPT_DIR}/${1}.tf"
+    CMD="sed"
+
+    declare -n VALS_REF=$2
+
+    for KEY in "${!VALS_REF[@]}"
+    do
+        CMD="${CMD} -e \"s/\\\${tfenv.${KEY}}/${VALS_REF[$KEY]}/g\""
+    done
+    CMD="${CMD} \"${SCRIPT_DIR}/${1}.tf.template\""
+    CMD="${CMD} > \"${SCRIPT_DIR}/${1}.tf\""
+
+    eval "$CMD"
 }
 
-generate backend
+generate backend VALS
 echo "backend.tf generated using values defined in .tfenv file."
-generate vars
+generate vars VALS
 echo "vars.tf generated using values defined in .tfenv file."
