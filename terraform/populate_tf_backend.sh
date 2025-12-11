e90738b8893b2da5a420c2a51b8166059b518fce
Donald Futers <donald.futers@gmail.com>
2025-12-11 15:56:40 +0000

    💡 add comments to populate_tf_backend.sh
    ️️♻️  reorganise populate_tf_backend.sh for greater clarity of logic

--- terraform/populate_tf_backend.sh
+++ terraform/populate_tf_backend.sh
@@ -1,37 +1,48 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")") 
-
-source "${SCRIPT_DIR}/.tfenv"
-
+# Fn: Escape special characters in strings for use in sed substitutions
 safe_escape () {
     printf '%s\n' "$1" | sed 's/[&/\]/\\&/g'
 }
 
+# Fn: Generate the target .tf using the provided associative array
+generate () {
+    # Retrieve args
+    TARGET_FILE="$1"
+    declare -n VALS_REF=$2
+
+    # Build the sed substitution command
+    CMD="sed"
+    for KEY in "${!VALS_REF[@]}"
+    do
+        CMD="${CMD} -e \"s/\\\${tfenv.${KEY}}/${VALS_REF[$KEY]}/g\""
+    done
+    CMD="${CMD} \"${SCRIPT_DIR}/${TARGET_FILE}.tf.template\""
+    CMD="${CMD} > \"${SCRIPT_DIR}/${TARGET_FILE}.tf\""
+
+    # Evaluate constructed command
+    eval "$CMD"
+}
+
+
+# Get the script's directory path
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")") 
+
+# Export .tfenv's values into the environment
+source "${SCRIPT_DIR}/.tfenv"
+
+# Extract the list of variable names defined in .tfenv
 VARS=( $(cat "${SCRIPT_DIR}/.tfenv" | grep -Po "\b(.+)(?==)") )
 
+# Zip names and values together into an associative array
 declare -A VALS
 for VAR in "${VARS[@]}"
 do
     VALS+=( ["$VAR"]="$( eval "safe_escape \"\$${VAR}\"" )" )
 done
 
-generate () {
-    CMD="sed"
-
-    declare -n VALS_REF=$2
-
-    for KEY in "${!VALS_REF[@]}"
-    do
-        CMD="${CMD} -e \"s/\\\${tfenv.${KEY}}/${VALS_REF[$KEY]}/g\""
-    done
-    CMD="${CMD} \"${SCRIPT_DIR}/${1}.tf.template\""
-    CMD="${CMD} > \"${SCRIPT_DIR}/${1}.tf\""
-
-    eval "$CMD"
-}
-
-generate backend VALS
+# Generate the .tfs
+generate "backend" VALS
 echo "backend.tf generated using values defined in .tfenv file."
-generate vars VALS
+generate "vars" VALS
 echo "vars.tf generated using values defined in .tfenv file."
