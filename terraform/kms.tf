092dab5a9039441413f495e4115e3d5f967ee015
Donald Futers <donald.futers@gmail.com>
2025-12-17 14:51:36 +0000

    🏔️ 🛡  TERRAFORM: add EFS permissions to KMS key policies for ephemeral and managed storage

--- terraform/kms.tf
+++ terraform/kms.tf
@@ -136,4 +136,30 @@ data "aws_iam_policy_document" "kms_key_for_fargate" {
             values = [ "Decrypt" ]
         }
     }
+
+    statement {
+        sid = "AllowEFSToUseKey"
+        effect = "Allow"
+        principals {
+            type = "Service"
+            identifiers = [ "fargate.amazonaws.com" ]
+        }
+        actions = [
+            "kms:Decrypt",
+            "kms:GenerateDataKeyWithoutPlaintext",
+            "kms:DescribeKey",
+            "kms:CreateGrant"
+        ]
+        resources = [ "*" ]
+        condition {
+            test = "StringEquals"
+            variable = "kms:ViaService"
+            values = [ "elasticfilesystem.${var.AWS_REGION}.amazonaws.com" ]
+        }
+        condition {
+            test = "StringEquals"
+            variable = "kms:CallerAccount"
+            values = [ "${data.aws_caller_identity.current.account_id}" ]
+        }
+    }
 }
