a855b9546e5e70e0e65ab7b6cbb4ada4bab2b222
Donald Futers <donald.futers@gmail.com>
2026-01-09 21:28:14 +0000

    🧪 LAMBDA: define tests for destroy_budibase_instance function

---
+++ test/test_destroy_budibase_instance.py
@@ -0,0 +1,69 @@
+import os, pytest, boto3, time
+from pprint import pprint
+from moto import mock_aws
+from unittest.mock import patch
+from src.destroy_budibase_instance import destroy_budibase_instance, lambda_handler
+
+
+@pytest.fixture(scope="function")
+def aws_credentials():
+    """Mocked AWS Credentials for moto"""
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"
+    os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
+
+
+@pytest.fixture(scope="module")
+def env_vars():
+    os.environ["TARGET_CLUSTER_NAME"] = "test-cluster"
+    os.environ["TARGET_SERVICE_NAME"] = "test-service"
+
+
+@pytest.fixture
+def ecs_client(aws_credentials, env_vars):
+    with mock_aws():
+        ecs = boto3.client("ecs")
+        yield ecs
+
+
+@pytest.fixture(scope="function")
+def ecs_with_cluster(ecs_client):
+    ecs_client.create_cluster(
+        clusterName = os.environ["TARGET_CLUSTER_NAME"]
+    )
+    ecs_client.create_service(
+        cluster = os.environ["TARGET_CLUSTER_NAME"],
+        serviceName = os.environ["TARGET_SERVICE_NAME"],
+        desiredCount = 1
+    )
+    yield ecs_client
+
+
+class TestCreateBudibaseInstanceFunction:
+    """ Unit tests for the create_budibase_instance() function. """
+
+    def test_service_desired_count_set_to_0(self, ecs_with_cluster):
+        destroy_budibase_instance(ecs_with_cluster)
+        result = ecs_with_cluster.describe_services(
+            cluster = os.environ["TARGET_CLUSTER_NAME"],
+            services = [os.environ["TARGET_SERVICE_NAME"]]
+        )
+        assert result["services"][0]["desiredCount"] == 0
+
+
+    def test_running_count_decreases_from_1_to_0(self, ecs_with_cluster):
+        result = ecs_with_cluster.describe_services(
+            cluster = os.environ["TARGET_CLUSTER_NAME"],
+            services = [os.environ["TARGET_SERVICE_NAME"]]
+        )
+        assert result["services"][0]["runningCount"] == 1
+
+        destroy_budibase_instance(ecs_with_cluster)
+
+        result = ecs_with_cluster.describe_services(
+            cluster = os.environ["TARGET_CLUSTER_NAME"],
+            services = [os.environ["TARGET_SERVICE_NAME"]]
+        )
+        assert result["services"][0]["runningCount"] == 0
