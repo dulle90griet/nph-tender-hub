c4110320f101923830106e6ec2404845b4733195
Donald Futers <donald.futers@gmail.com>
2026-01-22 20:20:09 +0000

    🏔️ 🔊 TERRAFORM: configure output for Budibase ECS list tasks command

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "lb_url" {
     description = "URL of load balancer"
     value       = "http://${aws_lb.budibase_alb.dns_name}/"
 }
+
+output "budibase_ecs_list_tasks_command" {
+    description = "Command to get list of Budibase TASK ARNs"
+    value       = "aws ecs list-tasks --cluster ${aws_ecs_cluster.budibase_cluster.name} --service ${aws_ecs_service.budibase_ecs_service.name}"
+}
