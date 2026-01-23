900d53280867321ca9b1197c7f3de86aafc543c5
Donald Futers <donald.futers@gmail.com>
2026-01-23 15:22:26 +0000

    🏔️ 🔊 TERRAFORM: configure output for Budibase ECS exec command

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,8 @@ output "budibase_ecs_list_tasks_command" {
     description = "Command to get list of Budibase TASK ARNs"
     value       = "aws ecs list-tasks --cluster ${aws_ecs_cluster.budibase_cluster.name} --service ${aws_ecs_service.budibase_ecs_service.name}"
 }
+
+output "budibase_ecs_exec_command" {
+    description = "Command to connect to ECS Exec shell on desired Budibase container instance"
+    value       = "aws ecs execute-command --region ${var.AWS_REGION} --cluster ${aws_ecs_cluster.budibase_cluster.name} --task [TASK ID FROM LIST-TASKS] --container ${var.PREFIX}-budibase-container --interactive --command '/bin/sh'"
+}
