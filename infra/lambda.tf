1899513d38ab7a8d4eca7be8f1567defdf6684b6
Donald Futers <donald.futers@gmail.com>
2026-02-18 00:53:58 +0000

    🐛 TERRAFORM: correct Lambda code S3 keys

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -9,7 +9,7 @@ resource "aws_lambda_function" "create_instance_lambda" {
   # filename      = data.archive_file.create_instance_lambda_code.output_path
   function_name = "${var.PREFIX}-${var.ENVIRONMENT}-create-instance-lambda"
   s3_bucket     = var.CODE_BUCKET
-  s3_key        = "create_instance_lambda/${var.LAMBDA_CREATE_SERVICE_VERSION}.zip"
+  s3_key        = "create_budibase_instance/${var.LAMBDA_CREATE_SERVICE_VERSION}.zip"
   role          = aws_iam_role.create_instance_lambda_execution_role.arn
   handler       = "create_budibase_instance.lambda_handler"
   # code_sha256   = data.archive_file.create_instance_lambda_code.output_base64sha256
@@ -37,7 +37,7 @@ resource "aws_lambda_function" "destroy_instance_lambda" {
   # filename      = data.archive_file.destroy_instance_lambda_code.output_path
   function_name = "${var.PREFIX}-${var.ENVIRONMENT}-destroy-instance-lambda"
   s3_bucket     = var.CODE_BUCKET
-  s3_key        = "destroy_lambda_function/${var.LAMBDA_DESTROY_SERVICE_VERSION}.zip"
+  s3_key        = "destroy_budibase_instance/${var.LAMBDA_DESTROY_SERVICE_VERSION}.zip"
   role          = aws_iam_role.destroy_instance_lambda_execution_role.arn
   handler       = "destroy_budibase_instance.lambda_handler"
   # code_sha256   = data.archive_file.destroy_instance_lambda_code.output_base64sha256
