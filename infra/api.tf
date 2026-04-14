a4a164eb74c77919b4c861b38d53ba716c78ea14
Donald Futers <donald.futers@gmail.com>
2026-04-14 17:25:26 +0100

    🏔️ 🚪 TF: add /service API routes

--- infra/api.tf
+++ infra/api.tf
@@ -69,6 +69,27 @@ resource "aws_apigatewayv2_route" "http_api_patch_consumable_route" {
   target = "integrations/${aws_apigatewayv2_integration.http_api_lambda_integration.id}"
 }
 
+resource "aws_apigatewayv2_route" "http_api_get_service_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "GET /service"
+
+  target = "integrations/${aws_apigatewayv2_integration.http_api_lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "http_api_post_service_route" {
+  api_id   = aws_apigatewayv2_api.http_api.id
+  route_key = "POST /service"
+
+  target = "integrations/${aws_apigatewayv2_integration.http_api_lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "http_api_patch_service_route" {
+  api_id   = aws_apigatewayv2_api.http_api.id
+  route_key = "PATCH /service/{service_id}"
+
+  target = "integrations/${aws_apigatewayv2_integration.http_api_lambda_integration.id}"
+}
+
 resource "aws_apigatewayv2_stage" "http_api_default_stage" {
   api_id      = aws_apigatewayv2_api.http_api.id
   name        = "$default"
