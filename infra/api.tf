dd636ec037358a98a05641942bbd5321469859e2
Donald Futers <donald.futers@gmail.com>
2026-04-24 17:59:11 +0100

    🏔️ 🚪 TF: add /client API routes

--- infra/api.tf
+++ infra/api.tf
@@ -182,6 +182,27 @@ resource "aws_apigatewayv2_route" "http_api_patch_direct_cost_route" {
   target = "integrations/${aws_apigatewayv2_integration.http_api_lambda_integration.id}"
 }
 
+resource "aws_apigatewayv2_route" "http_api_get_client_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "GET /client"
+
+  target = "integrations/${aws_apigatewayv2_integration.http_api_lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "http_api_post_client_route" {
+  api_id   = aws_apigatewayv2_api.http_api.id
+  route_key = "POST /client"
+
+  target = "integrations/${aws_apigatewayv2_integration.http_api_lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "http_api_patch_client_route" {
+  api_id   = aws_apigatewayv2_api.http_api.id
+  route_key = "PATCH /client/{client_id}"
+
+  target = "integrations/${aws_apigatewayv2_integration.http_api_lambda_integration.id}"
+}
+
 resource "aws_apigatewayv2_stage" "http_api_default_stage" {
   api_id      = aws_apigatewayv2_api.http_api.id
   name        = "$default"
