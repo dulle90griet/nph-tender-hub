f53ba7cd9e913991ce744def9203d54a5caf0cba
Donald Futers <donald.futers@gmail.com>
2026-08-27 00:55:14 +0100

    🖥️ ⚡BB: toggle ASC/DESC in sort config interface

---
+++ src/bb_queries/alternate_sort_direction.js
@@ -0,0 +1,7 @@
+// On-click code for the ASC / DESC toggle button pair in the sort config interface
+// Used in 'Update State' -> 'Set value' -> 'sortConditions'
+
+const conditions = JSON.parse($("State.sortConditions"));
+const index = $("[SortParams Repeater].[Row index]");
+conditions[index].direction = conditions[index].direction.toUpperCase() === "desc" ? "ASC" : "DESC";
+return conditions;
