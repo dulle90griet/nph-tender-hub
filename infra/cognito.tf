e7b561be5a651bab05979a04611d0bfa584f6e4c
Donald Futers <donald.futers@gmail.com>
2026-09-10 18:08:00 +0100

    🏔️ ️🛡️ 🔨 TF: update Cognito setup for m2m auth

--- infra/cognito.tf
+++ infra/cognito.tf
@@ -3,37 +3,59 @@ resource "aws_cognito_user_pool" "main" {
   deletion_protection = "ACTIVE"
   user_pool_tier      = "ESSENTIALS"
   alias_attributes    = ["email"]
+
+  lifecycle {
+    create_before_destroy = true
+    prevent_destroy       = true
+  }
 }
+
+resource "aws_cognito_user_pool_domain" "main" {
+  domain       = "${var.PREFIX}-${var.ENVIRONMENT}-user-pool-domain"
+  user_pool_id = aws_cognito_user_pool.main.id
+}
+
 
 resource "aws_cognito_user_pool_client" "budibase_m2m_client" {
   name = "${var.PREFIX}-${var.ENVIRONMENT}-budibase-client"
 
-  user_pool_id = aws_cognito_user_pool.main.id
-  # supported_identity_providers = ? (look into aws_cognito_identity_provider)
-  generate_secret = true
+  user_pool_id                  = aws_cognito_user_pool.main.id
+  generate_secret               = true
+  prevent_user_existence_errors = "ENABLED"
 
-  access_token_validity   = 1 # hours
+  access_token_validity   = 1
   enable_token_revocation = true
-  refresh_token_validity  = 5 #days
-  id_token_validity       = 1 # hours
+  refresh_token_validity  = 5
+  id_token_validity       = 1
   auth_session_validity   = 3 # minutes
 
-  explicit_auth_flows                  = ["ALLOW_REFRESH_TOKEN_AUTH"]
-  allowed_oauth_flows_user_pool_client = false
+  token_validity_units {
+    id_token      = "hours"
+    access_token  = "hours"
+    refresh_token = "days"
+  }
 
-  # read_attributes = leave as default?
-  # write_attributes = leave as default?
+  explicit_auth_flows                  = ["ALLOW_REFRESH_TOKEN_AUTH"]
+  allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_scopes                 = aws_cognito_resource_server.m2m_resource_server.scope_identifiers
+  allowed_oauth_flows                  = ["client_credentials"]
+  supported_identity_providers         = ["COGNITO"]
 
   enable_propagate_additional_user_context_data = false
-  prevent_user_existence_errors                 = "ENABLED"
+
+  lifecycle {
+    create_before_destroy = true
+    prevent_destroy       = true
+  }
 }
 
 resource "aws_apigatewayv2_authorizer" "budibase_m2m_authorizer" {
   name = "${var.PREFIX}-${var.ENVIRONMENT}-budibase-authorizer"
 
-  api_id                            = aws_apigatewayv2_api.http_api.id
-  authorizer_type                   = "JWT"
-  identity_sources                  = ["$request.header.Authorization"]
+  api_id           = aws_apigatewayv2_api.http_api.id
+  authorizer_type  = "JWT"
+  identity_sources = ["$request.header.Authorization"]
+
 
   jwt_configuration {
     audience = [aws_cognito_user_pool_client.budibase_m2m_client.id]
