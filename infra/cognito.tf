7dd33d4346acba494b9c3746a749f62327c262c2
Donald Futers <donald.futers@gmail.com>
2026-09-10 07:56:46 +0100

    🏔️ 🛡️  TF: add Cognito user pool client

--- infra/cognito.tf
+++ infra/cognito.tf
@@ -1,32 +1,32 @@
 resource "aws_cognito_user_pool" "main" {
-  name = "${var.PREFIX}-${var.ENVIRONMENT}-user-pool"
+  name                = "${var.PREFIX}-${var.ENVIRONMENT}-user-pool"
   deletion_protection = "ACTIVE"
-  user_pool_tier = "ESSENTIAL"
-  alias_attributes = "email"
+  user_pool_tier      = "ESSENTIALS"
+  alias_attributes    = ["email"]
 }
 
-# aws_cognito_user_pool_client
-##############################
-# user_pool_id connects to aws_cognito_user_pool
-# access_token_validity = 1 (default unit is hours)
-# allowed_oauth_flows_user_pool_client = true (allow OAuth 2.0 features)
-# allowed_oauth_flows = "code" / "implicit" / "client_credentials"?
-# allowed_oauth_scopes = "phone" / "email" / "openid" / "profile" / "aws.cognito.signin.user.admin"?
-# auth_session_validity = 3 (minutes)
-# callback_urls = ?
-# default_redirect_uri = ?
-# enable_token_revocation = true
-# enable_propagate_additional_user_context_data = false
-# explicit_auth_flows = ALLOW_REFRESH_TOKEN_AUTH and *maybe* ADMIN_NO_SRP_AUTH ?
-# generate_secret = true
-# id_token_validity = 1 (default unit is hours)
-# logout_urls = ?
-# prevent_user_existence_errors = "ENABLED"
-# read_attributes = leave as default?
-# refresh_token_rotation = ?
-# refresh_token_validity = 5 (default unit is days)
-# supported_identity_providers = ? (look into aws_cognito_identity_provider)
-# write_attributes = leave as default?
+resource "aws_cognito_user_pool_client" "budibase_m2m_client" {
+  name = "${var.PREFIX}-${var.ENVIRONMENT}-budibase-client"
+
+  user_pool_id = aws_cognito_user_pool.main.id
+  # supported_identity_providers = ? (look into aws_cognito_identity_provider)
+  generate_secret = true
+
+  access_token_validity   = 1 # hours
+  enable_token_revocation = true
+  refresh_token_validity  = 5 #days
+  id_token_validity       = 1 # hours
+  auth_session_validity   = 3 # minutes
+
+  explicit_auth_flows                  = ["ALLOW_REFRESH_TOKEN_AUTH"]
+  allowed_oauth_flows_user_pool_client = false
+
+  # read_attributes = leave as default?
+  # write_attributes = leave as default?
+
+  enable_propagate_additional_user_context_data = false
+  prevent_user_existence_errors                 = "ENABLED"
+}
 
 # aws_apigatewayv2_authorizer
 #############################
