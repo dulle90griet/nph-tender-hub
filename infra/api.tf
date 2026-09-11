739a1ac9dcc8dcd0a851115ad50c986de350deaa
Donald Futers <donald.futers@gmail.com>
2026-09-11 14:10:30 +0100

    🏔️ 🚪♻️  TF: move /client POST/PATCH to for_each

--- infra/api.tf
+++ infra/api.tf
@@ -111,19 +111,19 @@ resource "aws_apigatewayv2_route" "http_api_patch_direct_cost_route" {
   target = "integrations/${aws_apigatewayv2_integration.http_api_lambda_integration.id}"
 }
 
-resource "aws_apigatewayv2_route" "http_api_post_client_route" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "POST /client"
+# resource "aws_apigatewayv2_route" "http_api_post_client_route" {
+#   api_id    = aws_apigatewayv2_api.http_api.id
+#   route_key = "POST /client"
 
-  target = "integrations/${aws_apigatewayv2_integration.http_api_lambda_integration.id}"
-}
+#   target = "integrations/${aws_apigatewayv2_integration.http_api_lambda_integration.id}"
+# }
 
-resource "aws_apigatewayv2_route" "http_api_patch_client_route" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "PATCH /client/{client_id}"
+# resource "aws_apigatewayv2_route" "http_api_patch_client_route" {
+#   api_id    = aws_apigatewayv2_api.http_api.id
+#   route_key = "PATCH /client/{client_id}"
 
-  target = "integrations/${aws_apigatewayv2_integration.http_api_lambda_integration.id}"
-}
+#   target = "integrations/${aws_apigatewayv2_integration.http_api_lambda_integration.id}"
+# }
 
 resource "aws_apigatewayv2_route" "http_api_post_tender_route" {
   api_id    = aws_apigatewayv2_api.http_api.id
@@ -167,6 +167,8 @@ resource "aws_apigatewayv2_route" "http_api_get_routes" {
     "GET /labour-cost",
     "GET /direct-cost",
     "GET /client",
+    "POST /client",
+    "PATCH /client/{client_id}",
     "GET /client/names",
     "GET /tender",
     "GET /tender/single/{tender_id}",
