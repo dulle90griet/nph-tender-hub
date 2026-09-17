a9ed1c8e26468a3632bc3aad0f05ba6c85f1bad8
Donald Futers <donald.futers@gmail.com>
2026-09-17 17:48:11 +0100

    🔥 BB: delete deprecated add_search.js

--- src/bb_queries/search_interface/add_search.js
+++
@@ -1,9 +0,0 @@
-// On-click code for 'add search' button in the search interface.
-//
-// Used in 'Update State' -> 'Set value' -> 'sortConditions'.
-
-const conditions = JSON.parse($("State.searchValues"));
-if (conditions.length < 1) {
-  conditions.push({"searchColumn": "", "searchString": ""});
-}
-return JSON.stringify(conditions);
